0a2835bbad4081e132ee480473125d1e3961db04
n20010 <n20010@std.it-college.ac.jp>
2021-09-19 01:07:23 +0900

    finish section8 practice

--- scrapy_tutorial/BooksToScrape/BooksToScrape/spiders/books_fantasy.py
+++ scrapy_tutorial/BooksToScrape/BooksToScrape/spiders/books_fantasy.py
@@ -9,14 +9,26 @@ class BooksFantasySpider(scrapy.Spider):
     def parse(self, response):
         pods = response.xpath("//h3/a")
         for pod in pods:
-            title = pod.xpath(".//text()").get()
-            url = pod.xpath(".//@href").get()
-            yield {
-                "title" : title,
-                "url" : url
-            }
+        #    title = pod.xpath(".//text()").get()
+        #    url = pod.xpath(".//@href").get()
+        #    yield {
+        #        "title" : title,
+        #        "url" : url
+        #    }
+            yield response.follow(url = pod.xpath(".//@href").get(), callback=self.parse_item)
 
-        next = response.xpath('//li[@class="next"]/a/@href')
+        next = response.xpath('//li[@class="next"]/a/@href').get()
         if next:
             yield response.follow(url = next[0], callback=self.parse)
+    
+    def parse_item(self, response):
+        main_book_info = response.xpath('//div[@class="col-sm-6 product_main"]')
+        yield {
+            "title": main_book_info.xpath(".//h1/text()").get(),
+            "price": main_book_info.xpath('.//p[@class="price_color"]/text()').get(),
+            "stock": main_book_info.xpath('.//p[@class="instock availability"]/text()').get(),
+            "rating": response.xpath('//p[3]/@class').get(),
+            "UPC": response.xpath('//th[contains(text(), "UPC")]/following-sibling::td[1]/text()').get(),
+            "review": response.xpath('//th[contains(text(), "Number of reviews")]/following-sibling::td[1]/text()').get()
+        }
 
