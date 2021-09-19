9cee84f34eca6afcc64ef2ee08d9ba5067028df9
n20010 <n20010@std.it-college.ac.jp>
2021-09-19 20:48:26 +0900

    Fix a issue on make directory process

--- get_image/ebookorg/ebookorg/pipelines.py
+++ get_image/ebookorg/ebookorg/pipelines.py
@@ -12,7 +12,7 @@ from scrapy.pipelines.images import ImagesPipeline
 class customImagePipeline(ImagesPipeline):
     def file_path(self, request, response=None, info=None, *, item=None):
         name = request.url.split('/')[-1]
-        directory = item.get('title')[0]
+        directory = item.get('title')
         path = directory + r'\\' + name
         return path
 
