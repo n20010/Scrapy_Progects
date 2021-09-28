b3dc81dbc7f1f056ca7347c01d1db164a7bdc921
Koki Satani <n20010@std.it-college.ac.jp>
2021-09-29 05:00:32 +0900

    Add a feature read/export downloaded url file

--- get_image/ebookorg/ebookorg/spiders/get_images_from_search.py
+++ get_image/ebookorg/ebookorg/spiders/get_images_from_search.py
@@ -10,7 +10,7 @@ class GetImagesFromSearchSpider(scrapy.Spider):
     name = 'get_images_from_search'
 
     # Get target url and normalize it for scrapy here
-    start_url = input("add url: ")
+    start_url = input("[*] Add url: ")
     input_domains = re.sub('org.+$', 'org', start_url.replace('https://', ''))
     allowed_domains = [input_domains]
     start_urls = [start_url]
@@ -20,12 +20,34 @@ class GetImagesFromSearchSpider(scrapy.Spider):
     global titles
     titles = {}
 
+    global url_file_name
+    file_name = input('[*] Add URL file name you need: ')
+    url_file_name = f'../url_files/{file_name}.txt'
+
+    global downloaded_url_list
+    try:
+        with open(url_file_name, mode='r', encoding='utf-8') as f:
+            print(f"[*] Found {file_name}.txt. loading....")
+            downloaded_url_list = f.read().split('\n')
+    except FileNotFoundError:
+        with open(url_file_name, mode='w', encoding='utf-8') as f:
+            print(f"[*] NotFound {file_name}.txt. make new file.")
+            downloaded_url_list = [""]
+
+
+    print('[*] Opened downloaded urls list')
     # This method is running on main page
     def parse(self, response):
         # Here process implement to go to images pages, and next main page  
         index_list = response.xpath('//div[@class="glink"]/../@href').getall()
         for images_page in index_list:
-            yield response.follow(url=images_page, callback=self.parse_images)
+            if images_page not in downloaded_url_list:
+                print('[*] This page has not downloaded yet')
+                self.current_url_to_downloaded_list(images_page)
+                yield response.follow(url=images_page, callback=self.parse_images)
+            else:
+                print('[-] This page has already downloaded. Skip.')
+                print(f'[-] target URL: {response.url}')
 
         next = response.xpath('(//td[@class="ptds"]/following-sibling::td)[1]/a/@href').get()
         if next:
@@ -36,11 +58,10 @@ class GetImagesFromSearchSpider(scrapy.Spider):
         # Here process is judge this page is for japanese
         language = response.xpath('(//td[@class="gdt2"])[4]/text()').get()
         self.wait_time
-
+        title_main = self.normalize_title(response.xpath('//h1[@id="gj"]/text()').get())
         # If this page is for Japanese, we get image URLs from this page
         if re.match(r'Japanese.*$', language):
             title_key = self.normalize_title(response.xpath('//h1[@id="gn"]/text()').get())
-            title_main = self.normalize_title(response.xpath('//h1[@id="gj"]/text()').get())
             # Correct title can't get from next image page, so we add it to title assosiative directory here
             titles[title_key] = title_main
             images = response.xpath('//div[@class="gdtm"]/div/a/@href').getall()
@@ -50,7 +71,6 @@ class GetImagesFromSearchSpider(scrapy.Spider):
             # We got some embetted URLs from this page's images. so move there to get download URL for big size image
             for image in images:
                 yield response.follow(url=image, callback=self.get_image)
-
             # If this page include some pages for image, we move there and callback this method to get all images
             next_images = response.xpath('(//td[@class="ptds"]/following-sibling::td)[1]/a/@href').get()
             if next_images:
@@ -62,24 +82,32 @@ class GetImagesFromSearchSpider(scrapy.Spider):
 
     
     def get_image(self, response):
-        # add collect title and image download URL to Item Loader
-        # Item Loader send to Item Pipeline
-        # Item Pipeline get image file from added URL and save it our local computer
-        print('[*] get_image progress now')
-        print(f'[*] target URL: {response.url}')
-        self.wait_time
-        title_key = self.normalize_title(response.xpath('//div[@id="i1"]/h1/text()').get())
-        loader = ItemLoader(item=EbookorgItem(), response = response)
-        loader.add_value('title', titles[title_key])
-        loader.add_value('img_url',  response.xpath('//img[@id="img"]/@src').get())
-        yield loader.load_item()
+            # add collect title and image download URL to Item Loader
+            # Item Loader send to Item Pipeline
+            # Item Pipeline get image file from added URL and save it our local computer
+            print('[*] get_image progress now')
+            print(f'[*] target URL: {response.url}')
+            self.wait_time
+            title_key = self.normalize_title(response.xpath('//div[@id="i1"]/h1/text()').get())
+            loader = ItemLoader(item=EbookorgItem(), response = response)
+            loader.add_value('title', titles[title_key])
+            loader.add_value('img_url',  response.xpath('//img[@id="img"]/@src').get())
+            yield loader.load_item()
         
+
+    def current_url_to_downloaded_list(self, current_url):
+        if current_url not in downloaded_url_list:
+            with open(url_file_name, mode='a', encoding='utf-8') as f:
+                print(current_url, file=f)
+            print('[*] Current url write to downloaded url list')
+
 
     # when this method call, stop all process 
     # It is for data provider
     def wait_time(self):
         sleep(3)
     
+
     # We can customize title here
     def normalize_title(self, title):
         print(f'[*] in normalize progress: {title}')
